5da858d9292c30476036219791a47e5bc01805e3
mrich316 <mrich316@gmail.com>
2016-11-08 14:27:54 -0500

    feat: added landing page and moved api behind /api endpoint.

--- VagrantAtlas.WebApi/Startup.cs
+++ VagrantAtlas.WebApi/Startup.cs
@@ -1,15 +1,20 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.IO;
 using System.Net.Http.Headers;
 using System.Web.Hosting;
 using System.Web.Http;
 using System.Web.Http.Dispatcher;
+using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
 using Newtonsoft.Json;
 using Owin;
 using Thinktecture.IdentityModel.Owin;
 
 namespace VagrantAtlas.WebApi
 {
-    public class Startup
+    public static class Startup
     {
         public static void Configuration(IAppBuilder app)
         {
@@ -24,12 +29,12 @@ namespace VagrantAtlas.WebApi
 
             config.Routes.MapHttpRoute(
                 Constants.RouteNames.Default,
-                "{controller}/{id}",
+                "api/{controller}/{id}",
                 new { controller = "atlas", id = RouteParameter.Optional });
 
             config.Routes.MapHttpRoute(
                 Constants.RouteNames.Boxes,
-                "boxes/{user}/{name}/{version}",
+                "api/boxes/{user}/{name}/{version}",
                 new { controller = "boxes", version = RouteParameter.Optional });
 
             config.Formatters.Remove(config.Formatters.XmlFormatter);
@@ -50,7 +55,22 @@ namespace VagrantAtlas.WebApi
             config.Services.Replace(typeof(IHttpControllerActivator), new SingletonRepositoriesHttpControllerActivator(atlas));
 
             app.UseWebApi(config);
+            app.EnableStaticContent();
         }
 
+        private static void EnableStaticContent(this IAppBuilder app)
+        {
+            var root = AppDomain.CurrentDomain.BaseDirectory;
+            var physicalFileSystem = new PhysicalFileSystem(Path.Combine(root, "wwwroot"));
+            var options = new FileServerOptions
+            {
+                RequestPath = PathString.Empty,
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = false;
+            app.UseFileServer(options);
+        }
     }
 }
